4434ec7701660f100ee19dd412f08132f0f34f99
TamiresPaulino <tamirespaulino19@alu.ufc.br>
2023-05-26 10:41:44 -0300

    Versão Funcionando

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 """Module with functions."""
 
-
+import heapq
 def largest_sum(numbers: list[int]) -> tuple[int, int]:
     """Encontra e retorna os dois números que somados dão o maior valor."""
-    primeiro = 0  # o primeiro número da soma
-    segundo = 0  # o segundo número da soma
-    return primeiro, segundo
+    if len(numbers) < 2:
+        return None
+    
+    primeiro =max(numbers)  # o primeiro número da soma
+    segundo = heapq.nlargest(2, numbers)[-1]  # o segundo número da soma
+    return segundo, primeiro
+
+if __name__ == "__main__":
+    lista = largest_sum([-10, -5, -2, -1])
+    print(lista)
